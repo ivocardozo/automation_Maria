873e76f5876b5720b6c0fa6e5d3513ab3fac5a33
Maria Cardozo <isajocabet@gmail.com>
2023-04-03 00:43:55 -0400

    Proyecto MAria con 2 esenarios

--- config.app.ts
+++ config.app.ts
@@ -2,8 +2,8 @@ export const TESTDATA = {
     email: 'agv417@gmail.com',
     password: 'P@ssw0rd',
     // url: 'https://todoist.com/auth/login',
-    url: 'https://demoqa.com/text-box',
-    //url2: 'https://demoqa.com/webtables',
+    //ESCENARIO 1 url: 'https://demoqa.com/text-box',
+    url: 'https://demoqa.com/webtables',
     apiUrl: 'https://api.todoist.com/rest/v2/',
     apiToken: 'd574c2359cb59a80ac4fbfa4a2fcdf38a58cf06e',
 }
