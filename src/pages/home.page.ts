9edf6ec902d9da0da3d81f848a9230d44ca19851
Maria Cardozo <isajocabet@gmail.com>
2023-04-03 22:17:37 -0400

    trabajo final modulo 3

--- src/pages/home.page.ts
+++ src/pages/home.page.ts
@@ -1,5 +1,15 @@
+import { ElementActions } from "../core/element-actions";
 import { BasePage } from "./base.page";
+class HomePage extends BasePage {    
+    private elementButton: string = '//*[@id="app"]/div/div/div[2]/div/div[1]';
 
-class HomePage extends BasePage {
+    constructor(){
+        super();
+    }
 
+    async clickElements() {
+        await ElementActions.click(this.elementButton);
+    }
 }
+
+export const homePage = new HomePage();
---
+++ tests/steps/textBox.steps.ts
@@ -0,0 +1,43 @@
+import { Given, setDefaultTimeout, Then, When,  } from "@cucumber/cucumber";
+import { expect } from "chai";
+import { TESTDATA } from "../../config.app";
+import { homePage } from "../../src/pages/home.page"
+import { textBox } from "../../src/pages/components/textBox.page"
+
+setDefaultTimeout(60 * 1000);
+
+// Given('the user selects the Elements option', async function () {
+//     await homePage.clickElements();
+// });
+const sleep = (ms: number) => {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  };
+
+Given('the user clicks the text box button', async function () {
+        await textBox.clickTextBoxButton();
+});
+
+Given('the user sets {string} as Full Name', async function (inputname) {
+        await textBox.setNameTextField(inputname);
+        await sleep(1500);
+});
+
+Given('the user sets {string} as emailText', async function (inputemail) {
+    await textBox.setemailTextField(inputemail);
+    await sleep(1500);
+});
+
+Given('the user sets {string} as current Adress', async function (inputAdress) {
+    await textBox.setcurrentAddressTextField(inputAdress);
+    await sleep(1500);
+});
+
+Given('the user sets {string} as permanent Adress', async function (inputpermanentAdress) {
+    await textBox.setpermanentAddressTextField(inputpermanentAdress);
+    await sleep(1500);
+});
+
+Given('the user clicks as Submits button', async function () {
+    await textBox.clickSubmit();
+    await sleep(1500);
+});
